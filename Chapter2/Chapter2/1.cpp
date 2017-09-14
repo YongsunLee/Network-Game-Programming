ce502b8bc26dedf7dc7ad2f9865514f167805f55
YongsunLee <katarite715@naver.com>
2017-09-14 13:19:15 +0900

    연습문제
    연습문제 1번 비트연산 추가.

--- Chapter2/Chapter2/1.cpp
+++ Chapter2/Chapter2/1.cpp
@@ -10,14 +10,16 @@ int main(int argc, char *argv[])
 	WSADATA wsa1_1;
 
 	// 2.2 버전
+	// MAKEWORD(a, b)
+	// a가 상위 b가 하위
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
 		return 1;
 	}
 
 	// Ws2_32에서 호출자가 사용할것으로 예상하는 윈도우 사양 소켓
-	std::cout << "wVersion      : " << wsa.wVersion << std::endl;
+	std::cout << "wVersion      : " << (wsa.wVersion >> 8) << "." << (wsa.wVersion & 0x000f) << std::endl;
 	// Ws2_32가 지원할수 있는 최대 소켓 사양
-	std::cout << "wHighVersion  : " << wsa.wHighVersion << std::endl;
+	std::cout << "wHighVersion  : " << (wsa.wHighVersion >> 8) << "." << (wsa.wHighVersion & 0x000f) << std::endl;
 	// Ws2_32가 소켓 구현에 대한 설명을 복사하는 NULL로 끝나는 문자열
 	std::cout << "szDescription : " << wsa.szDescription << std::endl;
 	// Ws2_32이 관련상태 또는 구성 정보를 복사하는 NULL으로 끝나는 문자열
@@ -30,8 +32,8 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	std::cout << "wVersion      : " << wsa1_1.wVersion << std::endl;
-	std::cout << "wHighVersion  : " << wsa1_1.wHighVersion << std::endl;
+	std::cout << "wVersion      : " << (wsa1_1.wVersion >> 8) << "." << (wsa1_1.wVersion & 0x000f) << std::endl;
+	std::cout << "wHighVersion  : " << (wsa1_1.wHighVersion >> 8) << "." << (wsa1_1.wHighVersion & 0x00f) << std::endl;
 	std::cout << "szDescription : " << wsa1_1.szDescription << std::endl;
 	std::cout << "szSystemStatus: " << wsa1_1.szSystemStatus << std::endl;
 
